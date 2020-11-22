925d25a44ce4ac9937211d52f62952514d955f36
Owen Hunt <ophunt2000@gmail.com>
2020-11-21 20:04:02 -0600

    Created client and server folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 function App() {
+  console.log(process.env.NODE_ENV)
+  console.log(process.env.CLIENT_ID)
+
   return (
     <div className="App">
       <header>
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {process.env.CLIENT_ID}
         </p>
       </header>
     </div>
